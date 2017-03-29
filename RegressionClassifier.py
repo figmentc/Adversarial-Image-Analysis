343502448a0e0d2269507859829a3c37afe389cf
Austin Han <austin.han90@gmail.com>
2017-03-29 17:39:08 -0400

    Added Comments and Reorganized Code

--- RegressionClassifier.py
+++ RegressionClassifier.py
@@ -1,10 +1,11 @@
 from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 from numpy import *
 from scipy.misc import *
 import matplotlib.pyplot as plt
 import tensorflow as tf
+
 sess = tf.InteractiveSession()
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
 ### Google Tensorflow Sample Code 
 
@@ -44,84 +45,64 @@ for i in range(5000):
 
 ### Adversarial Image Analysis
 
-
+# Returns the tiled onehot ouput of classification pos from 0-9
+# int -> numpy array
 def create_tiled_onehot(pos):
     one_hot_i = zeros(10)
     one_hot_i[pos] = 1
     return tile(one_hot_i, (mnist.test.num_examples, 1))
+
+# Return the "shift" needed to generate the adversarial image as a factor of alpha 
+# and the gradient
+# int -> float32 tensor
+def calculate_delta(alpha):
     
-# Get correctly classified instances of label '2' prior to adversarial shift
-# Returns a boolean tensor to act as mask
+    #calculate gradient of the cost over input
+    grad_to_six = tf.gradients(cross_entropy, x)
+    grad_output = sess.run(grad_to_six, feed_dict={x:mnist.test.images, y_:create_tiled_onehot(6)})
+    
+    # delta calculated as gradient of the cost over input scaled by factor alpha
+    delta = alpha*grad_output[0]
+    return delta
+    
+# Return boolean mask for correctly classified instances of lablel '2' within the mnist test set
+# -> boolean tensor
 def correct_2_mask():
     label_2 = tf.equal(tf.argmax(y_,1), 2)
     correct_predict_2 = tf.logical_and(label_2, correct_prediction)
     return (sess.run(correct_predict_2, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
 
-# Calculate delta needed for adversarial shift
-def calculate_delta():
-    grad_to_six = tf.gradients(cross_entropy, x)
-    one_hot_six = zeros(10)
-    one_hot_six[6] = 1
-    grad_output = sess.run(grad_to_six, feed_dict={x:mnist.test.images, y_:create_tiled_onehot(6)})
-    delta = 1000*grad_output[0]
-    return delta
 
-# Get positive '6' classifications post adversarial shift
-# Returns a boolean tensor to act as mask
-def postshift_6_mask():
-    delta = calculate_delta()
+# Return boolean mask for classified instances of any label under under "6" after the adversarial shift
+# int -> boolean tensor
+def postshift_6_mask(alpha):
+    delta = calculate_delta(alpha)
     new_images = mnist.test.images - delta
     label_6 = tf.equal(tf.argmax(y,1), 6)
     return (sess.run(label_6, feed_dict={x: new_images, y_: mnist.test.labels}))
 
-    
-def build_results():
-    delta = reshape(calculate_delta(), (mnist.test.num_examples, 28,28))
+# Output the first 10 input under label '2' classified as '6' due to the adversarial shift
+# Returns the total number of input affected by adversarial shift (2 -> 6)
+# int -> int
+def build_results(alpha):
+    # Concatenate all original input, delta, and postshift_input
+    delta = reshape(calculate_delta(alpha), (mnist.test.num_examples, 28,28))
     prev_input = reshape(mnist.test.images, (mnist.test.num_examples, 28,28))
     postshift_input = reshape(mnist.test.images, (mnist.test.num_examples, 28,28)) - delta
     unfiltered = dstack((prev_input, delta ,postshift_input))
-    
 
-    combined_mask = tf.logical_and(correct_2_mask(), postshift_6_mask())
+    # Generate mask and filter for those affected
+    combined_mask = tf.logical_and(correct_2_mask(), postshift_6_mask(alpha))
     filtered = tf.boolean_mask(unfiltered,combined_mask)
-
     filtered_result = sess.run(filtered, feed_dict={})
-    print(filtered_result.shape)
+
+    # Reshape filtered_result to generate image from pixels
     shaped_filtered_result =  reshape(filtered_result, (filtered_result.shape[0]*28, 28*3))[:10*28]
-    # mask = tf.logical_and(correct_2_mask(), postshift_6_mask())
+    
     plt.imshow(shaped_filtered_result, cmap='gray')
     plt.show()
-    return 
-
-# We will change only correctly classified '2's
-
-# # Calculate gradient of cost towards the '6' classification over input
-# loss_to_six = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=y, labels=y_))
-# grad_to_six = tf.gradients(loss_to_six, x)
-# one_hot_six = zeros(10)
-# one_hot_six[6] = 1
-# grad_output = sess.run(grad_to_six, feed_dict={x:mnist.test.images, y_:tile(one_hot_six, (mnist.test.num_examples, 1))})
-# 
-# # Check accuracy from before and after changing input as a factor of the gradient
-# If overall accuracy increase, matches our intuition regarding the descent
-# six_prediction = tf.equal(tf.argmax(y,1), 6)
-# six_acc = tf.reduce_mean(tf.cast(six_prediction, tf.float32))
-# 
-# print("--Accuracy before descent towards '6':", 
-#         six_acc.eval(feed_dict={x: mnist.test.images, y_: tile(one_hot_six, (mnist.test.num_examples, 1))}))
-#         
-# new_images = mnist.test.images - 1000*grad_output[0]
-# 
-# print("--Accuracy after descent towards '6':", 
-#         six_acc.eval(feed_dict={x: new_images, y_: tile(one_hot_six, (mnist.test.num_examples, 1))}))
-
-
-
-# image1 = resize(mnist.test.images[1], (28,28))
-# # image2 = resize(new_images[1], (28,28))
-
-
-# plt.imshow(image1)
-# plt.show()
-# plt.imshow(image2)
-# plt.show()
+    
+    return filtered_result.shape[0]
+    
+if __name__ == __main__:
+    print(build_results(results(500)))
